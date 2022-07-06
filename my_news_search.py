1240bd70d02e53077305f630030fe213fea2c8a9
Ali Mehrban <ali.mehrb@outlook.com>
2022-07-06 12:59:47 +0430

    git

--- my_news_search.py
+++ my_news_search.py
@@ -5,7 +5,7 @@ apikey = 'fb0f4948b6a24d19b8ffcb5ef0f340e3'
 response = requests.get('https://newsapi.org/v2/everything',
                         params={
                             'q': 'Manchester United',
-                            'language': 'de',
+                            'language': 'de , en',
                             'sortBy': 'publishedAt',
                         },
                         headers={'X-Api-Key': apikey})
