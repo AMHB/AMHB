bee4975a8c3e71c0c463b8a811d7229c3e7552a0
Ali Mehrban <ali.mehrb@outlook.com>
2022-08-07 13:18:58 +0430

    print

---
+++ Guess_Game.py
@@ -0,0 +1,25 @@
+from ast import Break
+from ast import Continue
+#from pickle import TRUE
+import random
+
+i = 1
+j=99
+while True:
+
+  guess = random.randint(i,j)
+  # if guess <= j and guess >= i:
+  print(guess)
+  # else:
+  #   break
+
+  status = input("Please Clarify the status of guessed Number with k,b or d      ")
+  if status == "k":
+    j = guess
+    continue
+  if status == "b":
+    i = guess
+    continue
+  if status == "d":
+    print("Yesssss your guess is correct!!!")
+    break
